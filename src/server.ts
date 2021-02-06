334de1bade64a6b1e07bbce4316863a8ece478d1
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2021-02-06 15:26:56 -0300

    add jest

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import '@models/User';
+import '@models/User'
 import express from 'express'
 
 const app = express()
 
 app.get('/', (req, res) => {
-    return res.json({ msg: "teste" })
-});
+  return res.json({ msg: 'teste' })
+})
 
 app.listen(3333)
